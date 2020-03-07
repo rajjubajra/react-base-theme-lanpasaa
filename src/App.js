7847d3a742c0f50f4754ba1ea15ccf4fa306be75
rajju <vajrachara@yahoo.com>
2020-03-07 13:31:23 +0000

    copyright component added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.scss';
 import { BrowserRouter } from 'react-router-dom';
 import LanPasaa from './components/LanPasaa';
+import CopyRightFooter from './components/CopyRightFooter';
 
 
 function App() {
   return (
     <BrowserRouter>
       <LanPasaa />
+      <CopyRightFooter />
     </BrowserRouter>
   );
 }
